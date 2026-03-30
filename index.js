8cfc93615edbff46dbf156d2dc684be0c990f355
brunomiguel1120-sketch <brunomiguel1120@gmail.com>
2026-03-30 13:09:44 -0300

    Create index.js
    Bot

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
+
+const app = express();
+app.get("/", (req, res) => res.send("Bot online"));
+app.listen(3000, () => console.log("Web server rodando"));
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`Logado como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
